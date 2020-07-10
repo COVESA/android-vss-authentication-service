d5b595f85f9e0dcc50da223ac24ae83564738947
Stefan Wysocki <stefan.wysocki@tietoevry.com>
2020-07-10 17:19:11 +0200

    Add Apollo Grapqhl plugin
    Create 2 queries to check if its working.

--- ExampleAppJava/src/main/java/org/genivi/vss/authenticationservice/MainActivity.java
+++ ExampleAppJava/src/main/java/org/genivi/vss/authenticationservice/MainActivity.java
@@ -5,25 +5,33 @@
 
 package org.genivi.vss.authenticationservice.example;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.ComponentName;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+
+import org.genivi.vss.GetFuelDataQuery;
 import org.genivi.vss.authenticationservice.IAuthenticationService;
 import org.genivi.vss.authenticationservice.VSS;
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
     IAuthenticationService mAuthenticationService;
     private TextView mLogTextView;
+    private ApolloClient mApolloClient;
+    private String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,15 +53,21 @@ public class MainActivity extends AppCompatActivity {
             }
         }, BIND_AUTO_CREATE);
 
+
         Button button = findViewById(R.id.btn_request_value);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String token = getAuthenticationToken();
-                mLogTextView.append("Authentication token: " + token + "\n");
-                Log.d("VSS", "token: " + token);
-            }
+        button.setOnClickListener(v -> {
+            String token = getAuthenticationToken();
+            mLogTextView.append("\nAuthentication token: " + token + "\n");
+            Log.d(LOG_TAG, "token: " + token);
+            retrieveValue(token, (fuelLevel, tankCapacity) -> {
+                mLogTextView.append("Fuel level: " + fuelLevel + "\n");
+                mLogTextView.append("Tank capacity: " + tankCapacity + "\n");
+            });
         });
+
+        mApolloClient = ApolloClient.builder()
+                .serverUrl("http://192.168.56.101:4000/") // TODO make it configurable
+                .build();
     }
 
     private String getAuthenticationToken() {
@@ -64,5 +78,27 @@ public class MainActivity extends AppCompatActivity {
             mLogTextView.append("ERROR: Exception: " + e + "\n");
         }
         return null;
+    }
+
+    private void retrieveValue(String token, GetFuelDataCallback callback) {
+        mApolloClient.query(new GetFuelDataQuery()).enqueue(new ApolloCall.Callback<GetFuelDataQuery.Data>() {
+            @Override
+            public void onResponse(@NotNull Response<GetFuelDataQuery.Data> response) {
+                int level = response.getData().vehicle().drivetrain().fuelSystem().level();
+                int tankCapacity = response.getData().vehicle().drivetrain().fuelSystem().tankCapacity();
+                Log.d(LOG_TAG, "Response" + response.getData() + "\n");
+
+                callback.onResponse(level, tankCapacity);
+            }
+
+            @Override
+            public void onFailure(@NotNull ApolloException e) {
+                Log.e(LOG_TAG, "Error in executing getFuel query", e);
+            }
+        });
+    }
+
+    private interface GetFuelDataCallback {
+        void onResponse(int fuelLevel, int tankCapacity);
     }
 }
